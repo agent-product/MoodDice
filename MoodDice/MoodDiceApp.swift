2889fd1587bb86af230251c178e4a6fa3b945538
libaice <libaice147@gmail.com>
2025-08-26 17:01:17 +0800

    Initial Commit

---
+++ MoodDice/MoodDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodDiceApp.swift
+//  MoodDice
+//
+//  Created by baice on 8/26/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoodDiceTests/MoodDiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoodDiceTests.swift
+//  MoodDiceTests
+//
+//  Created by baice on 8/26/25.
+//
+
+import Testing
+@testable import MoodDice
+
+struct MoodDiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
